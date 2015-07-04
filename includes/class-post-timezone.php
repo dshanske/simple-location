8d1e74de42ee7dafb2c6865d6a369ed3fc5eec67
David Shanske <david@shanske.com>
2015-07-04 09:26:49 -0400

    Fix Filter for Time

--- includes/class-post-timezone.php
+++ includes/class-post-timezone.php
@@ -5,40 +5,37 @@ add_action( 'init' , array('post_timezone', 'init') );
 
 class post_timezone {
   public static function init() {
-		add_filter( 'get_the_date', array('post_timezone', 'get_the_date'), 12 );
-    add_filter( 'get_the_time', array('post_timezone', 'get_the_time'), 12 );
+		add_filter( 'get_the_date', array('post_timezone', 'get_the_date'), 12, 2 );
+    add_filter( 'get_the_time', array('post_timezone', 'get_the_time'), 12, 2 );
+		add_filter( 'get_the_modified_date' , array('post_timezone', 'get_the_date'), 12, 2);
+    add_filter( 'get_the_modified_time' , array('post_timezone', 'get_the_time')
+, 12, 2);
+
 	}
 
 	public static function get_the_date($the_date, $d = '' , $post = null) {
-		$post = get_post($post);
-		if ( ! $post ) {
-			return false;
-		}
+		$post = get_post( $post );
 	  $timezone = get_post_meta( $post->ID, '_timezone', true );
 		if ( !$timezone ) {
 			return $the_date;
 		}
 		if ( '' == $d ) {
-			$d = get_option('date_format');
+			$d = get_option( 'date_format' );
 		}
 		$datetime = new DateTime($post->post_date_gmt, new DateTimeZone('GMT'));
 		$datetime->setTimezone(new DateTimeZone($timezone));
-		$the_date = $datetime->format($d);
-		return $the_date;
+		return $datetime->format($d);
 	}
 
   public static function get_the_time($the_time, $d = '' , $post = null) {
-    $post = get_post($post);
-    if ( ! $post ) {
-      return false;
-    }
+		$post = get_post( $post );
     $timezone = get_post_meta( $post->ID, '_timezone', true );
     if ( !$timezone ) {
       return $the_time;
     }
     if ( '' == $d ) { 
-      $d = get_option('time_format');
-    }
+			$d = get_option( 'time_format' );
+		}	
     $datetime = new DateTime($post->post_date_gmt, new DateTimeZone('GMT'));
     $datetime->setTimezone(new DateTimeZone($timezone));
     $the_time = $datetime->format($d);
