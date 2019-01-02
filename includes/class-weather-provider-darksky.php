d9fe4ac6660d9797d8097e55c020f19c75fcf28d
Xavier Roy <xavierroy@users.noreply.github.com>
2019-01-02 15:16:51 +0530

    fix incorrect variable name
    reworded $time to $this

--- includes/class-weather-provider-darksky.php
+++ includes/class-weather-provider-darksky.php
@@ -88,7 +88,7 @@ class Weather_Provider_DarkSky extends Weather_Provider {
 				$return['visibility'] = $current['visibility'] * 1000;
 			}
 			$timezone          = Loc_Timezone::timezone_for_location( $this->latitude, $this->longitude );
-			$return['sunrise'] = sloc_sunrise( $time->latitude, $this->longitude, $timezone );
+			$return['sunrise'] = sloc_sunrise( $this->latitude, $this->longitude, $timezone );
 			$return['sunset']  = sloc_sunset( $this->latitude, $this->longitude, $timezone );
 
 			if ( $this->cache_key ) {
