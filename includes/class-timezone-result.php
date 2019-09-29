c2f06c2c205f3100e124c0b8b98014c247413cf0
David Shanske <david@shanske.com>
2019-09-29 05:56:18 +0000

    Remove _ as not needed

--- includes/class-timezone-result.php
+++ includes/class-timezone-result.php
@@ -3,33 +3,33 @@
 class Timezone_Result {
 	public $timezone = null;
 
-	private $_now;
-	private $_name;
+	private $now;
+	private $name;
 
 	public function __construct( $timezone, $date = false ) {
 		if ( $date ) {
-			$this->_now = new DateTime( $date );
+			$this->now = new DateTime( $date );
 		} else {
-			$this->_now = new DateTime();
+			$this->now = new DateTime();
 		}
-		$this->_now->setTimeZone( new DateTimeZone( $timezone ) );
-		$this->_name = $timezone;
+		$this->now->setTimeZone( new DateTimeZone( $timezone ) );
+		$this->name = $timezone;
 	}
 
 	public function __get( $key ) {
 		switch ( $key ) {
 			case 'offset':
-				return $this->_now->format( 'P' );
+				return $this->now->format( 'P' );
 			case 'seconds':
-				return (int) $this->_now->format( 'Z' );
+				return (int) $this->now->format( 'Z' );
 			case 'localtime':
-				return $this->_now->format( 'c' );
+				return $this->now->format( 'c' );
 			case 'name':
-				return $this->_name;
+				return $this->name;
 		}
 	}
 
 	public function __toString() {
-		return $this->_name;
+		return $this->name;
 	}
 }
