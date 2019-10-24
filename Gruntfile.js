804ad7a17d50875010e27d98b8def09f420514bc
David Shanske <david@shanske.com>
2019-10-24 01:15:22 +0000

    Remove checktextdomain

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,43 +7,8 @@ module.exports = function(grunt) {
                 src: ['js/location.js']
             }
         },
-        checktextdomain: {
-            options: {
-                text_domain: 'simple-location',
-                keywords: [
-                    '__:1,2d',
-                    '_e:1,2d',
-                    '_x:1,2c,3d',
-                    'esc_html__:1,2d',
-                    'esc_html_e:1,2d',
-                    'esc_html_x:1,2c,3d',
-                    'esc_attr__:1,2d',
-                    'esc_attr_e:1,2d',
-                    'esc_attr_x:1,2c,3d',
-                    '_ex:1,2c,3d',
-                    '_n:1,2,4d',
-                    '_nx:1,2,4c,5d',
-                    '_n_noop:1,2,3d',
-                    '_nx_noop:1,2,3c,4d'
-                ]
-            },
-            files: {
-                src: [
-                    '**/*.php', // Include all files
-                    'includes/*.php', // Include includes
-                    '!sass/**', // Exclude sass/
-                    '!node_modules/**', // Exclude node_modules/
-                    '!tests/**', // Exclude tests/
-                    '!vendor/**', // Exclude vendor/
-                    '!build/**' // Exclude build/
-                ],
-                expand: true
-            }
-        },
-
-
-
-        wp_readme_to_markdown: {
+        
+	wp_readme_to_markdown: {
             target: {
                 options: {
                     screenshot_url: '/assets/{screenshot}.png'
@@ -99,9 +64,8 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-wp-i18n');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-eslint');
-    grunt.loadNpmTasks('grunt-checktextdomain');
     grunt.loadNpmTasks('grunt-curl');
 
     // Default task(s).
-    grunt.registerTask('default', ['wp_readme_to_markdown', 'makepot', 'eslint', 'sass', 'checktextdomain']);
+    grunt.registerTask('default', ['wp_readme_to_markdown', 'makepot', 'eslint', 'sass' ]);
 };
