b25b5f5dfbd005a104df225a2a9d0e0b6ce84a67
David Shanske <david@shanske.com>
2020-05-11 03:03:20 +0000

    Add address to debug field.

--- templates/geocode-form.php
+++ templates/geocode-form.php
@@ -2,6 +2,8 @@
 <form method="get" action="<?php echo esc_url( rest_url( '/sloc_geo/1.0/geocode/' ) ); ?> ">
 	<p><label for="latitude"><?php esc_html_e( 'Latitude', 'simple-location' ); ?></label><input type="text" class="widefat" name="latitude" id="latitude" /></p>
 	<p><label for="longitude"><?php esc_html_e( 'Longitude', 'simple-location' ); ?></label><input type="text" class="widefat" name="longitude" id="longitude" /></p>
+	<p><?php esc_html_e( 'Or Lookup By Address', 'simple-location' ); ?></p>
+	<p><label for="address"><?php esc_html_e( 'Address', 'simple-location' ); ?></label><input type="text" class="widefat" name="address" id="address" /></p>
 	<?php wp_nonce_field( 'wp_rest' ); ?>
 	<?php submit_button( __( 'Lookup', 'simple-location' ) ); ?>
 </form>
