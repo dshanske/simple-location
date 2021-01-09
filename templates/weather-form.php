e5ff425d9bab53138c7936119a0bdabe617c3f94
David Shanske <david@shanske.com>
2021-01-09 16:01:01 +0000

    Add time field to weather debug form

--- templates/weather-form.php
+++ templates/weather-form.php
@@ -5,6 +5,7 @@
 	<p><label for="latitude"><?php esc_html_e( 'Latitude', 'simple-location' ); ?></label><input type="text" class="widefat" name="latitude" id="latitude" /></p>
 	<p><label for="longitude"><?php esc_html_e( 'Longitude', 'simple-location' ); ?></label><input type="text" class="widefat" name="longitude" id="longitude" /></p>
 	<p><label for="station"><?php esc_html_e( 'Station', 'simple-location' ); ?></label><input type="text" class="widefat" name="station" id="station" /></p>
+	<p><label for="time"><?php esc_html_e( 'Time', 'simple-location' ); ?></label><input type="text" class="widefat" name="time" id="time" /></p>
 	<p><label for="provider"><?php esc_html_e( 'Weather Provider', 'simple-location' ); ?></label><select name="provider">
 		<?php foreach ( $providers as $key => $value ) {
 			printf( '<option value="%1$s">%2$s</option>', $key, $value ); // phpcs:ignore
