9ec1cfd79f7f18e80bd44c89422f8c079dd56e61
David Shanske <david@shanske.com>
2021-02-14 01:25:22 +0000

    Add additional location tests

--- tests/test-location.php
+++ tests/test-location.php
@@ -1,7 +1,7 @@
 <?php
 
 class LocationTest extends WP_UnitTestCase {
-	public function test_set_and_get_location() {
+	public function test_set_and_get_location_data() {
 		$addr = array(
 				'country-name' => 'United States of America',
 				'country-code' => 'US',
@@ -12,6 +12,34 @@ class LocationTest extends WP_UnitTestCase {
 		$term = Location_Taxonomy::get_location( $addr, true );
 		$return = Location_Taxonomy::location_data_to_hadr( Location_Taxonomy::get_location_data( $term ) );
 		$this->assertEquals( $addr, $return, wp_json_encode( $return, JSON_PRETTY_PRINT ) );
+		wp_delete_term( $term, 'location' );
+	}
+
+	public function test_set_and_get_location() {
+		$addr = array(
+				'country-name' => 'United States of America',
+				'country-code' => 'US',
+				'region' => 'Pennsylvania',
+				'region-code' => 'PA',
+				'locality' => 'Philadelphia'
+			);
+		$term = Location_Taxonomy::get_location( $addr, true );
+		$termtoo = Location_Taxonomy::get_location( $addr, true );
+		$this->assertEquals( $term, $termtoo );
+		wp_delete_term( $term, 'location' );
+	}
+
+	public function test_set_and_get_locality() {
+		$addr = array(
+				'country-name' => 'United States of America',
+				'country-code' => 'US',
+				'region' => 'Pennsylvania',
+				'region-code' => 'PA',
+				'locality' => 'Philadelphia'
+			);
+		$term = Location_Taxonomy::get_location( $addr, true );
+		$locality = Location_Taxonomy::get_locality( $addr );
+		$this->assertEquals( $term, $locality );
 		wp_delete_term( $term, 'location' );
 	}
 }
