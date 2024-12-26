80da8e98f205e92a6477ef1b6af56e6183047f6d
David Shanske <david@shanske.com>
2024-12-26 13:24:35 +0000

    Move SASS processing to composer from Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
                 src: ['js/location.js', 'js/zones.js' ]
             }
         },
-        
+
 	wp_readme_to_markdown: {
             target: {
                 options: {
@@ -28,26 +28,6 @@ module.exports = function(grunt) {
 			],
 		},
 	},
-        sass: {
-            dev: {
-                options: {
-                    style: 'expanded'
-                },
-                files: {
-                    'css/location.css': 'sass/main.scss',
-                    'css/location-admin.css': 'sass/admin.scss'
-                }
-            },
-            dist: {
-                options: {
-                    style: 'compressed'
-                },
-                files: {
-                    'css/location.min.css': 'sass/main.scss',
-                    'css/location-admin.min.css': 'sass/admin.scss'
-                }
-            }
-        },
         downloadfile: {
 	    options: {
 	       overwriteEverytime: true
@@ -55,17 +35,16 @@ module.exports = function(grunt) {
 	    files: {
               'data/airports.csv': 'https://davidmegginson.github.io/ourairports-data/airports.csv',
 	      'data/airlines.csv': 'https://raw.githubusercontent.com/jpatokal/openflights/master/data/airlines.dat',
-            } 
+            }
        }
     });
 
     // Load plugins.
     grunt.loadNpmTasks('grunt-wp-readme-to-markdown');
-    grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-eslint');
     grunt.loadNpmTasks('grunt-downloadfile');
 
     // Default task(s).
-    grunt.registerTask('default', ['wp_readme_to_markdown', 'eslint', 'sass' ]);
+    grunt.registerTask('default', ['wp_readme_to_markdown', 'eslint' ]);
 };
