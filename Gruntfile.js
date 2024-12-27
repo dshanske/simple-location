2f000f3d3966fbde6382b0b79b054a7c01fbcd94
David Shanske <david@shanske.com>
2024-12-27 06:40:20 +0000

    Move download file option to composer from Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,23 +27,13 @@ module.exports = function(grunt) {
 				{expand: true, cwd: 'node_modules/simple-icons/icons/', src: ['{boeing,lufthansa,britishairways,easyjet,americanairlines,s7airlines,unitedairlines,pegasusairlines,ethiopianairlines,southwestairlines,lotpolishairlines,chinaeasternairlines,chinasouthernairlines,aerlingus,aeroflot,aeromexico,aircanada,airchina,airfrance,airasia,airbus,emirates,etihadairways,qatarairways,ryanair,sanfranciscomunicipalrailway,shanghaimetro,turkishairlines,wizzair,alitalia,ana,delta}.svg'], dest: 'svgs/'},
 			],
 		},
-	},
-        downloadfile: {
-	    options: {
-	       overwriteEverytime: true
-            },
-	    files: {
-              'data/airports.csv': 'https://davidmegginson.github.io/ourairports-data/airports.csv',
-	      'data/airlines.csv': 'https://raw.githubusercontent.com/jpatokal/openflights/master/data/airlines.dat',
-            }
-       }
+	}
     });
 
     // Load plugins.
     grunt.loadNpmTasks('grunt-wp-readme-to-markdown');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-eslint');
-    grunt.loadNpmTasks('grunt-downloadfile');
 
     // Default task(s).
     grunt.registerTask('default', ['wp_readme_to_markdown', 'eslint' ]);
